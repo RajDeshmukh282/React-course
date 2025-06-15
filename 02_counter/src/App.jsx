fd8f56cf6bfc81441e614d19aee4c9e1b742a63b
Raj Deshmukh <rajdeshmukh282@gmail.com>
2025-06-15 13:30:39 +0530

    Implement counter functionality with increment and decrement buttons

--- 02_counter/src/App.jsx
+++ 02_counter/src/App.jsx
@@ -1,14 +1,32 @@
-
+import React from 'react'
+import { useState } from 'react';
 import './App.css'
 
+
+
+
 function App() {
+  const addvalue = () => {
+  counter = counter +1;
+  setCounter(counter);
+  console.log("Counter value after increment: ", counter);  
+}
+function decrementvalue() {
+  counter = counter - 1;
+  setCounter(counter);
+  console.log("Counter value after decrement: ", counter);
+}
+  // let counter  = 5;
+let [counter , setCounter] = useState(5);
 
   return (
     <>
       <h1>React With Raj</h1>
-      <h2>Counter value : 5</h2>
-      <button>Increment  value</button>
-      <button>Decrement value</button>
+      <h2>Counter value : {counter} </h2>
+      <button onClick={addvalue}>Increment  value</button>
+      <br />
+      <br />
+      <button onClick={decrementvalue}>Decrement value</button>
     </>
   )
 }
